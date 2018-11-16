ff328e4530e3a3b65c6d1ca40e67351aa85fed59
Sébastien Eustace <sebastien@eustace.io>
2018-11-16 13:13:52 -0500

    Fix tests

--- tests/console/conftest.py
+++ tests/console/conftest.py
@@ -17,7 +17,6 @@ from poetry.packages import Locker as BaseLocker
 from poetry.repositories import Pool
 from poetry.repositories import Repository
 from poetry.utils._compat import Path
-from poetry.utils.env import MockEnv
 from poetry.utils.toml_file import TomlFile
 
 
@@ -49,13 +48,6 @@ def installed():
 
 @pytest.fixture(autouse=True)
 def setup(mocker, installer, installed, config):
-    mocker.patch(
-        "poetry.utils.env.EnvManager.get",
-        return_value=MockEnv(
-            path=Path("/prefix"), base=Path("/base/prefix"), is_venv=True
-        ),
-    )
-
     # Set Installer's installer
     p = mocker.patch("poetry.installation.installer.Installer._get_installer")
     p.return_value = installer
